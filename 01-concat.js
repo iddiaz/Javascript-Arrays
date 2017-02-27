b0aba7c3ac08588d45218c0348144e193a6fb60f
iddiaz <diazdiazivan@gmail.com>
2017-02-27 17:52:56 +0100

    concat uses

---
+++ 01-concat.js
@@ -0,0 +1,36 @@
+//Array.prototype.concat();
+
+/**
+ * EL metodo concat de los arrays, añade datos a un array incluso unifica arrays
+ */
+
+var items = [1, 2];
+
+var newItems = items.concat(3, 4, 5, 'strings', undefined);
+var newItems = items.concat([2, 4, 5]);
+var newItems = items.concat([2, 4, 5], [8, 8, 8]);
+var newItems = items.concat([2, 4, 5], [8, 8, 8, [9, 10]]);
+console.log(newItems);
+
+//Array de Objetos
+
+var people = [{
+      name: 'Shane',
+   },
+   {
+      name: 'Shally'
+   }
+];
+var people2 = [{
+      name: 'Simon',
+   },
+   {
+      name: 'Ben'
+   }
+];
+
+people
+   .concat(people2)
+   .forEach(function (person) {
+      console.log(person.name);
+   })
